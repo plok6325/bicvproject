1cb844a4deaaeaa458a35877cbd766586ef54221
hongbin <plok6325@gmail.com>
2017-05-25 01:29:36 +0100

    first commit

---
+++ myplot.py
@@ -0,0 +1,83 @@
+
+
+'''this is file contain functions of plotting things '''
+
+
+import matplotlib.pyplot as plt
+from scipy.misc import imresize
+
+def plot_comparison(O_image, down_image):
+    plt.subplot(2,3,1)
+    plt.imshow(O_image)
+    plt.title('original size')
+
+    plt.subplot(2,3,2)
+    imrecov = imresize(down_image,size=(28,28),interp='nearest')
+    plt.imshow(imrecov)
+    plt.title('nearest ')
+
+    plt.subplot(2,3,3)
+    imrecov = imresize(down_image,size=(28,28),interp='lanczos')
+    plt.imshow(imrecov)
+    plt.title('lanczos ')
+
+    plt.subplot(2,3,4)
+    imrecov = imresize(down_image,size=(28,28),interp='bilinear')
+    plt.imshow(imrecov)
+    plt.title('bilinear ')
+
+    plt.subplot(2,3,5)
+    imrecov = imresize(down_image,size=(28,28),interp='cubic')
+    plt.imshow(imrecov)
+    plt.title('cubic ')
+
+
+    plt.subplot(2,3,6)
+    imrecov = imresize(down_image,size=(28,28),interp='bicubic')
+    plt.imshow(imrecov)
+    plt.title('bicubic ')
+
+    plt.show()
+
+
+def plot_2(original,SR):
+    plt.subplot(211)
+    plt.title('original')
+    plt.imshow(original)
+    plt.subplot(212)
+    plt.title('SR')
+    plt.imshow(SR)
+    plt.show()
+
+def plot_3(O_image,down_image,SR):
+
+    plt.subplot(2,3,1)
+    plt.imshow(O_image)
+    plt.title('original size')
+
+    plt.subplot(2,3,2)
+    imrecov = imresize(down_image,size=(28,28),interp='nearest')
+    plt.imshow(imrecov)
+    plt.title('nearest ')
+
+    plt.subplot(2,3,3)
+    imrecov = imresize(down_image,size=(28,28),interp='lanczos')
+    plt.imshow(imrecov)
+    plt.title('lanczos ')
+
+    plt.subplot(2,3,4)
+    imrecov = imresize(down_image,size=(28,28),interp='bilinear')
+    plt.imshow(imrecov)
+    plt.title('bilinear ')
+
+    plt.subplot(2,3,5)
+    imrecov = imresize(down_image,size=(28,28),interp='cubic')
+    plt.imshow(imrecov)
+    plt.title('cubic ')
+
+
+    plt.subplot(2,3,6)
+    plt.imshow(SR)
+    plt.title('Super R ')
+
+    plt.show()
